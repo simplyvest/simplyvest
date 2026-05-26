c7b510ee123720b84ff1f6310a118401f897c6d1
Calvin S <calvinsntf@gmail.com>
2026-05-26 16:14:14 +0700

    Create use-program.ts

---
+++ apps/web/app/hooks/use-program.ts
@@ -0,0 +1,20 @@
+import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import * as anchor from "@coral-xyz/anchor";
+import { useMemo } from "react";
+
+import idl from "@/idl/solana_tdp.json";
+import type { SolanaTdp } from "@/idl/solana_tdp.types";
+
+export function useProgram() {
+  const wallet = useAnchorWallet();
+  const { connection } = useConnection();
+
+  const program = useMemo(() => {
+    if (!wallet) return null;
+
+    const provider = new anchor.AnchorProvider(connection, wallet, {});
+    return new anchor.Program<SolanaTdp>(idl as SolanaTdp, provider);
+  }, [wallet, connection]);
+
+  return program;
+}
